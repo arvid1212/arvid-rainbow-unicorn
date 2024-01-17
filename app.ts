9244a6ff689ff5a0cb87afdd5626e11f0711d8b8
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-01-17 12:07:24 +0100

    fourth

--- app.ts
+++ app.ts
@@ -1,3 +1,26 @@
-const animals = ["apa","orangutang","gorilla","get","giraff"];
+var animals = ["apa","orangutang","gorilla","get","giraff"];
     
 console.log(animals);
+
+
+
+type Movie = {
+ director : string;
+ year: number;
+ seen: boolean;
+}
+
+
+let Brazil: Movie =  {
+director: 'Terry Gilliam',
+year:1985,
+seen:false,
+
+};
+console.log(Brazil);
+
+var namn = "";
+
+function hello(namn){
+    console.log("hej" + namn)
+}
